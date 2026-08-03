aaf277c922b84d2ee3634fedfd50ae4187424a95
Nikita Shybeka <nikitichsn2@gmail.com>
2026-08-03 19:16:32 +0300

    feat: add input component

---
+++ src/components/input/input.tsx
@@ -0,0 +1,10 @@
+import type { ComponentProps } from "react"
+import { Input as ShadcnInput } from "@/shared/ui/shadcn/input"
+
+export interface InputProps extends ComponentProps<typeof ShadcnInput> {
+  error?: boolean
+}
+
+export function Input({ error, ...props }: InputProps) {
+  return <ShadcnInput aria-invalid={!!error} {...props} />
+}
