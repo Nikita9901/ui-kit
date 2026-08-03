d82d24cbf1b5df916834a3823f45cd45553a73d1
Nikita Shybeka <nikitichsn2@gmail.com>
2026-08-03 19:08:04 +0300

    feat: add button component

---
+++ src/components/button/button.tsx
@@ -0,0 +1,10 @@
+import type { ComponentProps } from "react"
+import { Button as ShadcnButton } from "@/shared/ui/shadcn/button"
+
+export interface ButtonProps extends ComponentProps<typeof ShadcnButton> {
+  error?: boolean
+}
+
+export function Button({ error, ...props }: ButtonProps) {
+  return <ShadcnButton aria-invalid={!!error} {...props} />
+}
