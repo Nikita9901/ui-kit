8a97144e373f30de51b211cb63f2b990337fa14f
Nikita Shybeka <nikitichsn2@gmail.com>
2026-08-03 20:51:16 +0300

    test: add button stories with interaction tests

---
+++ src/components/button/button.stories.tsx
@@ -0,0 +1,103 @@
+import type { Meta, StoryObj } from "@storybook/react-vite"
+import { expect, fn, userEvent, within } from "storybook/test"
+// import { userEvent as realUserEvent } from "vitest/browser"
+
+import { Button } from "./button"
+
+const meta = {
+  title: "Components/Button",
+  component: Button,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    variant: {
+      control: "select",
+      options: [
+        "default",
+        "outline",
+        "secondary",
+        "ghost",
+        "destructive",
+        "link",
+      ],
+    },
+    size: {
+      control: "select",
+      options: [
+        "default",
+        "xs",
+        "sm",
+        "lg",
+        "icon",
+        "icon-xs",
+        "icon-sm",
+        "icon-lg",
+      ],
+    },
+  },
+  args: {
+    children: "Button",
+    onClick: fn(),
+  },
+} satisfies Meta<typeof Button>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {}
+
+// export const Hovered: Story = {
+//   play: async ({ canvasElement }) => {
+//     const canvas = within(canvasElement)
+//     const button = canvas.getByRole("button")
+//
+//     const initialBackground = getComputedStyle(button).backgroundColor
+//
+//     // TODO:поменять на проверку цветов когда сделаю тему и цвета
+//
+//     await realUserEvent.hover(button)
+//
+//     await expect(getComputedStyle(button).backgroundColor).not.toBe(
+//       initialBackground
+//     )
+//   },
+// }
+
+export const Focused: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    const button = canvas.getByRole("button")
+
+    await userEvent.tab()
+
+    await expect(button).toHaveFocus()
+  },
+}
+
+export const ErrorState: Story = {
+  args: {
+    error: true,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    const button = canvas.getByRole("button")
+
+    await expect(button).toHaveAttribute("aria-invalid", "true")
+  },
+}
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    const button = canvas.getByRole("button")
+
+    await expect(button).toBeDisabled()
+    await expect(button).toHaveStyle({ pointerEvents: "none" })
+  },
+}
